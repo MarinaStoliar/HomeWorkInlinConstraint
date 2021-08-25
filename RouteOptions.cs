58f8a27244914f9be398b8d96dc59dc51d6a0b6e
MarinaStoliar <stoliarmarina39@gmail.com>
2021-08-25 16:15:43 +0300

    first commit

---
+++ RouteOptions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InlinConstraint
+{
+    public class RouteOptions
+    {
+        public bool LowercaseUrls { get; set; }
+        public bool AppendTrailingSlash { get; set; }
+        private IDictionary<string, Type> _constraintTypeMap = GetDefaultConstraintMap();
+        public IDictionary<string, Type> ConstraintMap
+        {
+            get
+            {
+                return _constraintTypeMap;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(ConstraintMap));
+                }
+
+                _constraintTypeMap = value;
+            }
+        }
+
+        private static IDictionary<string, Type> GetDefaultConstraintMap()
+        {
+            return new Dictionary<string, Type>(StringComparer.OrdinalIgnoreCase)
+            {
+                // Type-specific constraints
+                { "int", typeof(IntRouteConstraint) },
+                { "bool", typeof(BoolRouteConstraint) },
+                { "datetime", typeof(DateTimeRouteConstraint) },
+                { "decimal", typeof(DecimalRouteConstraint) },
+                { "double", typeof(DoubleRouteConstraint) },
+                { "float", typeof(FloatRouteConstraint) },
+                { "guid", typeof(GuidRouteConstraint) },
+                { "long", typeof(LongRouteConstraint) },
+
+                // Length constraints
+                { "minlength", typeof(MinLengthRouteConstraint) },
+                { "maxlength", typeof(MaxLengthRouteConstraint) },
+                { "length", typeof(LengthRouteConstraint) },
+
+                // Min/Max value constraints
+                { "min", typeof(MinRouteConstraint) },
+                { "max", typeof(MaxRouteConstraint) },
+                { "range", typeof(RangeRouteConstraint) },
+
+                // Regex-based constraints
+                { "alpha", typeof(AlphaRouteConstraint) },
+                { "regex", typeof(RegexInlineRouteConstraint) },
+
+                {"required", typeof(RequiredRouteConstraint) },
+            };
+        }
+    }
+}
